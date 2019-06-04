a46e5fcbf2ad1891a4b786424560b0986c561f72
naiimayy <naiiaramay@gmail.com>
2019-06-04 17:00:55 -0300

    Banco de dados e Repositório desenvolvidos

---
+++ BancoDadosFarmacia/Repositório/RepositorioProdutoHigienico.cs
@@ -0,0 +1,46 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositório
+{
+    public class RepositorioProdutoHigienico
+    {
+        string CadeiaConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\65979\Documents\ExercicioFarmacia.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public List<ProdutoHigienico> ObterTodos()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexao;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = "SELECT * FROM produtos_higienicos";
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            List<ProdutoHigienico> produtosHigienicos = new List<ProdutoHigienico>();
+
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                DataRow linha = tabela.Rows[i];
+                ProdutoHigienico produtoHigienico = new ProdutoHigienico();
+                produtoHigienico.Id = Convert.ToInt32(linha["id"]);
+                produtoHigienico.Nome = linha["nome"].ToString();
+                produtoHigienico.Preco = Convert.ToDecimal(linha["preco"]);
+                produtoHigienico.Categoria = linha["categoria"].ToString();
+                produtosHigienicos.Add(produtoHigienico);
+            }
+            conexao.Close();
+            return produtosHigienicos;
+
+        }
+    }
+}
