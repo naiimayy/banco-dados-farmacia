a74b9d9aba94059fc26d86c02ea8e0de08de5bde
naiimayy <naiiaramay@gmail.com>
2019-06-06 17:18:10 -0300

    Iniciado desenvolvimento do Repositório Remédio.

--- BancoDadosFarmacia/Repositório/RemedioRepositorio.cs
+++ BancoDadosFarmacia/Repositório/RemedioRepositorio.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,35 @@ namespace Repositório
 {
     public class RemedioRepositorio
     {
+        string CadeiaConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\65979\Documents\ExercicioFarmacia.mdf;Integrated Security=True;Connect Timeout=30";
+    }
+
+    public List<Remedio> ObterTodos ()
+    {
+        SqlConnection conexao = new SqlConnection();
+        conexao.ConnectionString = CadeiaConexao;
+        conexao.Open();
+
+        SqlCommand comando = new SqlCommand();
+        comando.Connection = conexao;
+        comando.CommandText = "SELECT * FROM remedios";
+
+        DataTable tabela = new DataTable();
+        tabela.Load(comando.ExecuteReader());
+
+        List<Remedio> remedios = new List<Remedio>();
+        for (int i = 0; i < tabela.Rows.Count; i++)
+        {
+            DataRow linha = tabela.Rows[i];
+            Remedio remedio = new Remedio();
+            remedio.Id = Convert.ToInt32(linha["id"]);
+            remedio.Generico = ;
+            remedio.Nome = linha["nome"].ToString();
+            remedio.Categoria = linha["categoria"].ToString();
+            remedio.Solido = ;
+            remedio.Bula = linha["bula"].ToString();
+            remedio.Faixa = linha["faixa"].ToString();
+            remedio.Receita = ;
+        }
     }
 }
