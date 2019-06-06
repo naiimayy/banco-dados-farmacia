1b603e7d1ff445ce10c68129240f7704e63a741a
naiimayy <naiiaramay@gmail.com>
2019-06-06 17:06:37 -0300

    Classe Remédio criado.

--- BancoDadosFarmacia/Model/Remedio.cs
+++ BancoDadosFarmacia/Model/Remedio.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Remedio
+    public class Remedio
     {
+        public int Id;
+        public bool Generico;
+        public string Nome;
+        public string Categoria;
+        public bool Solido;
+        public string Bula;
+        public string Faixa;
+        public bool Receita;
     }
 }
--- BancoDadosFarmacia/Repositório/RemedioRepositorio.cs
+++ BancoDadosFarmacia/Repositório/RemedioRepositorio.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repositório
 {
-    class RemedioRepositorio
+    public class RemedioRepositorio
     {
     }
 }
