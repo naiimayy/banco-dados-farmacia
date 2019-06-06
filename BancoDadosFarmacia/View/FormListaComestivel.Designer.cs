cad0add40484c33b9b9331505ea3e6cd50117dd6
naiimayy <naiiaramay@gmail.com>
2019-06-06 16:00:31 -0300

    Criado Form Lista Comestivel - Atualizar Tabela, Apagar, Cadastrar, DataGridView Cell Double Click e Form Lista Comestivel Activated

--- BancoDadosFarmacia/View/FormListaComestivel.Designer.cs
+++ BancoDadosFarmacia/View/FormListaComestivel.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPreco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDataVencimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnQuantidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMarca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnNome,
+            this.ColumnPreco,
+            this.ColumnDataVencimento,
+            this.ColumnQuantidade,
+            this.ColumnMarca});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(642, 376);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "Id";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.ReadOnly = true;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.ReadOnly = true;
+            // 
+            // ColumnPreco
+            // 
+            this.ColumnPreco.HeaderText = "Preço";
+            this.ColumnPreco.Name = "ColumnPreco";
+            this.ColumnPreco.ReadOnly = true;
+            // 
+            // ColumnDataVencimento
+            // 
+            this.ColumnDataVencimento.HeaderText = "DataVencimento";
+            this.ColumnDataVencimento.Name = "ColumnDataVencimento";
+            this.ColumnDataVencimento.ReadOnly = true;
+            // 
+            // ColumnQuantidade
+            // 
+            this.ColumnQuantidade.HeaderText = "Quantidade";
+            this.ColumnQuantidade.Name = "ColumnQuantidade";
+            this.ColumnQuantidade.ReadOnly = true;
+            // 
+            // ColumnMarca
+            // 
+            this.ColumnMarca.HeaderText = "Marca";
+            this.ColumnMarca.Name = "ColumnMarca";
+            this.ColumnMarca.ReadOnly = true;
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(498, 42);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(75, 23);
+            this.btnApagar.TabIndex = 4;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(579, 42);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 3;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // FormListaComestivel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(672, 459);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "FormListaComestivel";
             this.Text = "FormListaComestivel";
+            this.Activated += new System.EventHandler(this.FormListaComestivel_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPreco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDataVencimento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnQuantidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMarca;
     }
 }
--- BancoDadosFarmacia/View/FormListaComestivel.cs
+++ BancoDadosFarmacia/View/FormListaComestivel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repositório;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,49 @@ namespace View
         public FormListaComestivel()
         {
             InitializeComponent();
+        }
+
+        private void AtualizarTabela()
+        {
+            ComestivelRepositorio repositorio = new ComestivelRepositorio();
+            List <Comestivel> comestiveis = repositorio.ObterTodos();
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < comestiveis.Count; i++)
+            {
+                Comestivel comestivel = comestiveis[i];
+                dataGridView1.Rows.Add(new object[] { comestivel.Id, comestivel.Nome, comestivel.Preco, comestivel.DataVencimento, comestivel.Quantidade, comestivel.Marca});
+            }
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+
+            ComestivelRepositorio repositorio = new ComestivelRepositorio ();
+            repositorio.Apagar(id);
+            dataGridView1.Rows.RemoveAt(dataGridView1.CurrentCell.RowIndex);
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            FormCadastroComestivel cadastro = new FormCadastroComestivel();
+            cadastro.ShowDialog();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+
+            ComestivelRepositorio repositorio = new ComestivelRepositorio();
+            Comestivel comestivel = repositorio.ObterPeloId(id);
+
+            FormEditarCadastroComestivel formEditarCadastroComestivel = new FormEditarCadastroComestivel(comestivel);
+            formEditarCadastroComestivel.ShowDialog();
+        }
+
+        private void FormListaComestivel_Activated(object sender, EventArgs e)
+        {
+            AtualizarTabela();
         }
     }
 }
